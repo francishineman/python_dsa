12edab3e10884f109fd4f1b34dddb5699e16bade
Francis Hineman <65574457+francishineman@users.noreply.github.com>
2025-10-04 09:57:31 -0700

    Update two_sum_brute.py

--- leet_code/0001_two_sum/two_sum_brute.py
+++ leet_code/0001_two_sum/two_sum_brute.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
 
+target = 9
+nums = [2,7,10,13,15,22,45]
+
+def brute_force_solution(tgt, nms):
+    for i in nms:
+        for j in nms:
+            if i + j == tgt:
+                return([i, j])
+
+def main():
+    answer = brute_force_solution(target, nums)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
