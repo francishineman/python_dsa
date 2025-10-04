108be1ab749a20fffab504088573498c3756dbb5
Francis Hineman <65574457+francishineman@users.noreply.github.com>
2025-10-04 09:56:27 -0700

    Update two_sum_hash.py

--- leet_code/0001_two_sum/two_sum_hash.py
+++ leet_code/0001_two_sum/two_sum_hash.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
 
+target = 9
+nums = [2,7,10,13,15,22,45]
+
+def hash_table_algorithm(tgt, nms):
+    d = {}
+
+    for i in nums:
+        compliment = tgt - i
+        if i in d:
+            return(i,compliment)
+        else:
+            d[compliment] = i
+
+
+def main():
+    answer = hash_table_algorithm(target, nums)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
